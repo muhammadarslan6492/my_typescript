eae256ebfb7907e319d8e6c479d67f86fdcd91b1
muhammad <muhammadarslan6492@gmail.com>
2022-10-21 12:45:53 +0500

    section 2

---
+++ section2/interfaces.ts
@@ -0,0 +1,38 @@
+interface Veicle {
+  name: string;
+  year: Date;
+  broken: boolean;
+  sumary(): string;
+}
+
+interface Reports {
+  sumary(): string;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: new Date(),
+  broken: true,
+  sumary(): string {
+    return `Name: ${this.name}, year: ${this.year}, isBroken: ${this.broken}`;
+  },
+};
+
+const drink = {
+  name: 'Pepsi',
+  color: 'black',
+  sumary(): string {
+    return `Drink: ${this.name}, color: ${this.color}`;
+  },
+};
+
+const printVeicle = (vehicle: Veicle): void => {
+  console.log(vehicle.sumary());
+};
+
+const report = (item: Reports): void => {
+  console.log(item.sumary());
+};
+
+printVeicle(oldCivic);
+report(drink);
